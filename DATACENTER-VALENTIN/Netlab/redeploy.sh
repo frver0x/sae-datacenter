7a635eb052819b399c31df855e508edffd6087e9
Ver0X <82282116+frver0x@users.noreply.github.com>
2026-06-25 12:16:34 +0200

    ajout de fichiers

--- DATACENTER-VALENTIN/Netlab/redeploy.sh
+++
@@ -1,9 +0,0 @@
-ssh-keygen -R 192.168.121.101
-ssh-keygen -R 192.168.121.102
-ssh-keygen -R 192.168.121.103
-ssh-keygen -R 192.168.121.104
-ssh-keygen -R 192.168.121.105
-ssh-keygen -R 192.168.121.106
-
-netlab status -i default --cleanup
-netlab up 
